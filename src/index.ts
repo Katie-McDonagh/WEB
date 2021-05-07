ee9943137cff8f7bb09217705ada6424bbd805ef
Katie McDonagh <mcdonagh.katie@outlook.com>
2021-05-07 09:18:15 +0100

    make each property of userprops interface optional

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { User } from './models/User'
 
 const user = new User({ name: 'Katie', age: 30 })
 
-user.set({ name: 'new name', age: 999 })
+user.set({ name: 'new name' })
 console.log(user.get('name'))
 console.log(user.get('age'))
