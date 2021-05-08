1d7ea37739685d50394d587547551b6557ee9b09
Katie McDonagh <mcdonagh.katie@outlook.com>
2021-05-08 12:12:09 +0100

    add trigger events

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import { User } from './models/User'
 
 const user = new User({ name: 'Katie', age: 30 })
 
-user.set({ name: 'new name' })
-console.log(user.get('name'))
-console.log(user.get('age'))
+user.on('change', () => {
+  console.log('1')
+})
+user.on('save', () => {
+  console.log('save was triggered')
+})
+
+user.trigger('change')
--- src/models/User.ts
+++ src/models/User.ts
@@ -4,7 +4,7 @@ interface UserProps {
 }
 
 // type alias
-type Callback = () => {}
+type Callback = () => void
 
 export class User {
   //  below is assigning events to an object where the key is a string and the value is a callback function
@@ -16,10 +16,25 @@ export class User {
   }
 
   set(update: UserProps): void {
-    Object.assign(this.data, update);
+    Object.assign(this.data, update)
   }
 
-  on(eventName: string, callback: Callback) {
+  on(eventName: string, callback: Callback): void {
+    //will be either a callback[] or undefined 
+    const handlers = this.events[eventName] || []
+    handlers.push(callback)
+    this.events[eventName] = handlers
+  }
 
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return
+    }
+
+    handlers.forEach(callback => {
+      callback()
+    })
   }
 }
